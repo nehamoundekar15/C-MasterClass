f5a98e43bbf8a5cbdbd15e349001cc258285f837
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-07 18:31:49 +0530

    Factorial of Number

---
+++ FactorialOfNum/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace FactorialOfNum
+{
+    internal class Program
+    {
+        public static int Factorial(int number)
+        {
+            int fact = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********************************************");
+            Console.WriteLine("Factorial of Number");
+            Console.WriteLine("Result of Factorial: "+ Factorial(5));
+            Console.ReadKey();
+        }
+    }
+}
