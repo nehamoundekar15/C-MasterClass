865847155889954c306c0b40af8a3a6c8ea6efa7
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-07 18:18:13 +0530

    RepeatCharacter

---
+++ RepeatCharacterTillTargetLengeth/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RepeatCharacterTillTargetLengeth
+{
+    internal class Program
+    {
+        public static string RepeatCharacter(char character, int targetLength)
+        {
+            string result = "";
+            do
+            {
+                result += character;
+            }
+            while (result.Length < targetLength);
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************************************************");
+            Console.WriteLine("RepeatCharacter till targeted length");
+            Console.WriteLine("Result: "+ RepeatCharacter('*', 4));
+            Console.ReadKey();  
+        }
+    }
+}
