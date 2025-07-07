e8f873710ee5d0a80ad3a2eefd7b7b8bad6e10e5
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-07 17:57:21 +0530

    CalculateSumOfNumbersBetween

---
+++ CalculateSumOfNumbersBetweenEx/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CalculateSumOfNumbersBetweenEx
+{
+    internal class Program
+    {
+        public static int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+        {
+            int result = 0;
+            //your code goes here
+            while (firstNumber <= lastNumber)
+            {
+                result = result + firstNumber;
+                firstNumber++;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******************************************************");
+            Console.WriteLine("While loop - CalculateSumOfNumbersBetween");
+            int sumOfNum = CalculateSumOfNumbersBetween(5, 10);
+            Console.WriteLine("Result: "+sumOfNum);
+            Console.ReadLine();
+        }
+    }
+}
