1514abf02591768121db7660f18f7f21a5e59dd1
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-07 16:52:41 +0530

    switch with return keyword instead on break;

--- ConvertPointsToGrade/Program.cs
+++ ConvertPointsToGrade/Program.cs
@@ -2,40 +2,39 @@
 {
     internal class Program
     {
+        static string GetGrade(int points)
+        {
+            switch (points)
+            {
+                case 10:
+                case 9:
+                    return "A grade";
+                case 8:
+                case 7:
+                case 6:
+                    return "B grade";
+                case 5:
+                case 4:
+                case 3:
+                    return "C grade";
+                case 2:
+                case 1:
+                    return "D grade";
+                case 0:
+                    return "Fail";
+                default:
+                    return "Invalid points";
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("*******************************************");
             Console.WriteLine("Convert points to grade");
             Console.WriteLine("Enter ponits");
             int userPoints = Convert.ToInt32(Console.ReadLine());
-
-            switch (userPoints)
-            {
-                case 10:
-                case 9:
-                    Console.WriteLine("A grade"); 
-                    break;
-                case 8:
-                case 7:
-                case 6:
-                    Console.WriteLine("B grade");
-                    break;
-                case 5:
-                case 4:
-                case 3:
-                    Console.WriteLine("C grade");
-                    break;
-                case 2:
-                case 1:
-                    Console.WriteLine("D grade");
-                    break;
-                case 0:
-                    Console.WriteLine("E grade");
-                    break;
-                default:
-                    Console.WriteLine("!");
-                    break;
-            }
+            Console.WriteLine(GetGrade(userPoints));
+            
             Console.ReadLine();
         }
     }
