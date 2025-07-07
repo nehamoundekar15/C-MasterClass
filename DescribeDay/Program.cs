50bc92dcc8de339a5674c8ad476d9131b99ecf43
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-07 17:29:55 +0530

    day describe with switch expression

--- DescribeDay/Program.cs
+++ DescribeDay/Program.cs
@@ -4,21 +4,31 @@
     {
         public static string DescribeDay(int dayNumber)
         {
-            switch (dayNumber)
-            {
+            //switch staements
+            //switch (dayNumber)
+            //{
 
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                    return "Working day";
-                case 6:
-                case 7:
-                    return "Weekend";
-                default:
-                    return "Invalid day number";
-            }
+            //    case 1:
+            //    case 2:
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //        return "Working day";
+            //    case 6:
+            //    case 7:
+            //        return "Weekend";
+            //    default:
+            //        return "Invalid day number";
+            //}
+
+
+            //switch expression - more concise and more readable format 
+            return dayNumber switch
+            {
+                1 or 2 or 3 or 4 or 5 => "Working day",
+                6 or 7 => "Weekend", 
+                _ => "Invalid day number"
+            };
 
         }
         static void Main(string[] args)
