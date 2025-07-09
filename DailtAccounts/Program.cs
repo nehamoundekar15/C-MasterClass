5671577caa3d4c53288fdb1bc674ee13d7b718da
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-09 16:21:37 +0530

    DailyAccounts

---
+++ DailtAccounts/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DailtAccounts
+{
+    internal class Program
+    {
+        public class DailyAccountState
+        {
+            public int InitialState { get; }
+
+            public int SumOfOperations { get; }
+
+            public DailyAccountState(
+                int initialState,
+                int sumOfOperations)
+            {
+                InitialState = initialState;
+                SumOfOperations = sumOfOperations;
+            }
+
+            //your code goes here
+            public int EndOfDayState => InitialState + SumOfOperations;
+
+            public string Report =>
+            $"Day: {DateTime.Now.Day}," +
+            $"month: {DateTime.Now.Month}," +
+            $"year: {DateTime.Now.Year}," +
+            $"initial state: {InitialState}, " +
+            $"end of day state: {EndOfDayState}";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******************************************************");
+            DailyAccountState dailyAccountState = new DailyAccountState(1000, -200);
+            Console.WriteLine(dailyAccountState.Report);
+            Console.ReadLine();
+        }
+    }
+}
